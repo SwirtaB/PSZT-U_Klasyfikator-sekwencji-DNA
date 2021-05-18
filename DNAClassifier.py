990c19afc60c27d0b8579089985a27283c0173fc
SwirtaB <bartosz.swirta@gmail.com>
2021-05-18 13:09:19 +0200

    ID3 works, tests are required

--- DNAClassifier.py
+++ DNAClassifier.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import sklearn
 import sys
+import pprint
 
 def readSpliceFile(filePath, atrNumber):
     try:
@@ -42,7 +43,7 @@ def get_entropy(divideAttribute, collection):
 
 def get_inf_gain(divideAttribute, collection):
     uniqueAttributes = collection[divideAttribute].unique()
-    print(uniqueAttributes)
+    # print(uniqueAttributes)
     collectionGrouped = collection.groupby([divideAttribute])
     information = 0
     for attribute in uniqueAttributes:
@@ -51,6 +52,50 @@ def get_inf_gain(divideAttribute, collection):
     
     return get_entropy('class', collection) - information
 
-data = pd.DataFrame({'x1' : ['A', 'B', 'B', 'B', 'B'], 'x2' : [1, 1, 2, 2, 3], 'class' : [0, 1, 1, 0, 1]} )
-print(get_inf_gain('x1', data))
-print(get_inf_gain('x2', data))
+def choose_best_attribute(collection, classLabel):
+    maxInfGain = 0
+    bestAttribute = None
+    for attribute in collection.keys():
+        if attribute != classLabel:
+            infGain = get_inf_gain(attribute, collection)
+            if infGain > maxInfGain:
+                bestAttribute = attribute
+                maxInfGain = infGain
+            elif infGain == maxInfGain:
+                bestAttribute = attribute
+                maxInfGain = infGain
+
+    return bestAttribute
+        
+
+def build_ID3(collection : pd.DataFrame, classLabel, ID3Tree = None):
+    if ID3Tree is None:
+        ID3Tree = {}
+
+    uniqueClasses = collection[classLabel].value_counts()
+    if len(uniqueClasses) == 1:
+        return uniqueClasses.index[0]
+
+    if len(collection.keys()) == 1:
+        return uniqueClasses.index[0]
+
+    bestAttribute = choose_best_attribute(collection, classLabel)
+    uniqueValues = collection[bestAttribute].unique()
+    ID3Tree[bestAttribute] = {}
+    for value in uniqueValues:
+        subCollection = collection[collection[bestAttribute] == value].reset_index(drop=True) #dzieli DataFrame zgodnie z wartością wybranego atrybutu
+        subCollection = subCollection.drop(bestAttribute, axis=1) #pozbywamy się wyciętej kolumny
+        ID3Tree[bestAttribute][value] = build_ID3(subCollection, classLabel)
+
+    return ID3Tree
+        
+data = readSpliceFile("Data/spliceDTrainKIS.txt", 15)
+       
+# data = pd.DataFrame({'x1' : ['A', 'B', 'B', 'B', 'B'], 'x2' : [1, 1, 2, 2, 3], 'class' : [0, 1, 1, 0, 1]} )
+# print(get_inf_gain('x1', data))
+# print(get_inf_gain('x2', data))
+# pprint.pprint(build_ID3(data, "class"))
+
+tree = build_ID3(data, "class")
+# print(tree['x1']['B']['x2'][3])
+pprint.pprint(tree)
